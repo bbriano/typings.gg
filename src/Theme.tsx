9419f163c7b3fdf936ce7d1e0a91929aa51d122b
Briano Goestiawan <b@briano.io>
2021-03-08 15:44:17 +1100

    Add theme type

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -2,7 +2,31 @@ import { createContext, useContext } from "react";
 import "./App.css";
 
 type Theme = {
-  // a: number
+  main: {
+    background: string
+    foreground: string
+  }
+
+  card: {
+    background: string
+    foreground: string
+  }
+
+  wordHighlights: {
+    current: string
+    correct: string
+    wrong: string
+  }
+
+  textBox: {
+    background: string
+    foreground: string
+  }
+
+  redoButton: {
+    background: string
+    foreground: string
+  }
 };
 
 const ThemeContext = createContext<Theme>({});

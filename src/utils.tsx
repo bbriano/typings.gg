12e8fb69151f01b02d60dfca8073f851517b1dc8
spaaaacccee <kaiwen530@gmail.com>
2021-03-15 16:09:28 +1100

    Improve row and add column components

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,9 +1,46 @@
 import { ReactNode } from "react";
+import styles from "./utils.module.css";
 
-export function Row({ children }: { children: ReactNode }) {
+type Alignment = "flex-start" | "center" | "flex-end";
+
+type Props = {
+  children?: ReactNode;
+  align?: {
+    vertical?: Alignment;
+    horizontal?: Alignment;
+  };
+};
+
+export function Row({
+  children,
+  align: { vertical = "center", horizontal = "center" } = {},
+}: Props) {
   return (
-    <div>
-      { children }
+    <div
+      className={styles.row}
+      style={{
+        justifyContent: horizontal,
+        alignItems: vertical,
+      }}
+    >
+      {children}
     </div>
-  )
+  );
+}
+
+export function Column({
+  children,
+  align: { vertical = "center", horizontal = "center" } = {},
+}: Props) {
+  return (
+    <div
+      className={styles.column}
+      style={{
+        justifyContent: vertical,
+        alignItems: horizontal,
+      }}
+    >
+      {children}
+    </div>
+  );
 }
